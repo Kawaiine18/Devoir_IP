9356319ffe45f520c619c5aa8afaba0e611545e5
Kawaiine18 <167618690+Kawaiine18@users.noreply.github.com>
2024-09-14 18:16:44 +0300

    Update ip-1.c

--- ip-1.c
+++ ip-1.c
@@ -1,15 +1,15 @@
 void getdata(char** ip)
 {
-  *ip = getenv("QUERY_STRING");
-   if(*ip == NULL)
-   {
-	printf("Content-type:Text\html\n\n");
-	printf("Erreur!");
-   }
+	*ip = getenv("QUERY_STRING");
+  	if(*ip == NULL)
+  	{
+		printf("Content-type:Text\html\n\n");
+		printf("<p>Erreur!</p>");
+  	}
 } 
 void affichage(char* ip,int x,int y,int z,int w)
 {
-    printf("Content-type:Text/html \n\n");
+	printf("Content-type:Text/html \n\n");
     printf("<HTML>");
     printf("<HEAD><TITLE>ip</TITLE></HEAD>");
     printf("<BODY>");
@@ -22,21 +22,21 @@ void affichage(char* ip,int x,int y,int z,int w)
         printf("<p>l'adresse ip invalide</p>");
     }
     else
-    {
-      if(x >= 0 && x <= 126)
-      {
-          printf("<p>Adresse est de classe A</p>");
-      }
-      if(x >= 128 && x <= 191)
-      {
-         printf("<p>Adresse est de classe B</p>");
-      }
-      if(x >= 192 && x <= 223)
-      {
-      	 printf("<p>Adresse est de classe C</p>");
-      }
-    }
-    printf("</BODY>");
-    printf("</HTML>");
+	{
+		if(x >= 0 && x <= 126)
+     	{
+        	 printf("<p>Adresse est de classe A</p>");
+     	}
+     	if(x >= 128 && x <= 191)
+     	{
+        	printf("<p>Adresse est de classe B</p>");
+     	}
+     	if(x >= 192 && x <= 223)
+     	{
+      		printf("<p>Adresse est de classe C</p>");
+     	}
+   	}
+   	printf("</BODY>");
+  	printf("</HTML>");
     return 0;
 }
